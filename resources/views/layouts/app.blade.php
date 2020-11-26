6dc7be97e92131145b8a6b9a5f1a710a9f488919
mostafa hassan <mostafahassan999888@gmail.com>
2020-11-26 12:14:47 +0200

    bug fix
    fixed a bug showing an error when unlogged in user try to access the website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="user_id" content="{{Auth::user()->id}}">
+    <meta name="user_id" content="{{Auth::user() ? Auth::user()->id : ''}}">
+    
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
